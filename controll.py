2595975db82669573a44b10de3594fa8524a8fb3
AskerYC <2391090197@qq.com>
2020-02-14 19:34:44 +0800

    a

--- controll.py
+++ controll.py
@@ -17,52 +17,52 @@ def index1():
         return send_file("./css/style.css")
         
 @app.route('/img/01.png')
-def index1():
+def index2():
         return send_file("./img/01.png")
 
 @app.route('/img/02.png')
-def index1():
+def index3():
         return send_file("./img/02.png")
         
 @app.route('/img/03.png')
-def index1():
+def index4():
         return send_file("./img/03.png")
         
 @app.route('/img/04.png')
-def index1():
+def index5():
         return send_file("./img/04.png")
 
 @app.route('/img/05.png')
-def index1():
+def index6():
         return send_file("./img/05.png")
         
 @app.route('/img/06.png')
-def index1():
+def index7():
         return send_file("./img/06.png")
         
 @app.route('/img/1.png')
-def index1():
+def index8():
         return send_file("./img/1.png")
 
 @app.route('/img/2.png')
-def index1():
+def index9():
         return send_file("./img/2.png")
         
 @app.route('/img/3.png')
-def index1():
+def index10():
         return send_file("./img/3.png")
         
 @app.route('/img/4.png')
-def index1():
+def index11():
         return send_file("./img/4.png")
 
 @app.route('/img/5.png')
-def index1():
+def index12():
         return send_file("./img/5.png")
         
 @app.route('/img/6.png')
-def index1():
+def index13():
         return send_file("./img/6.png")
         
 if __name__ == '__main__':
-        app.run(host='0.0.0.0', port=8081)
+        app.run(host='0.0.0.0', port=8082)
